be9f4dfe250411687fa308e3be010fd726560e93
FlipFlaptheTraitor <flipflapthetraitor@protonmail.com>
2021-01-24 21:41:33 -0500

    JS file added vars set for main function

---
+++ Assets/Javascript/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  //sets format for dates
+  const currenttime = moment().format('MMMM Do YYYY');
+    let currentTimeHourAM = moment().format('H');
+    let currentTimeHourPM = moment().format('h');
+  
+
+})
